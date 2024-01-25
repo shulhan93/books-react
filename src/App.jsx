57380fa104af7e439b1745192f22ba4f7493467f
shulgan-andrei <eng.shulhan.andrei@gmail.com>
2024-01-25 09:24:06 +0300

    Added BookForm, BookList and Filter Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import BookForm from "./components/BookForm/BookForm";
+import Filter from "./components/Filter/Filter";
+import BookList from "./components/BookList/BookList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library</h1>
+      </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          {/* <Filter /> */}
+          <BookList />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/BookForm/BookForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./BookForm.css";
+
+export default function BookForm() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      console.log(title, author);
+      setTitle("");
+      setAuthor("");
+    }
+  };
+
+  return (
+    <div className="app-block book-form">
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="author">Title: </label>
+          <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+}
